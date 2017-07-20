2752a28f3cf5b7b78c2e0f455555768cad52df55
Matthew Menna <mattmenna@gmail.com>
2017-07-20 10:19:30 -0400

    Initial commit

--- lab19.js
+++ lab19.js
@@ -1,7 +1,7 @@
-var price = [0.50, 1.99, 3.99, 1.00]
+var price = ["0.50", "1.99", "3.99", "1.00"]
 
 function getItems(){
-
+        document.getElementById("total").innerHTML ="price[i]";
 }
 
 function getPrice() {
