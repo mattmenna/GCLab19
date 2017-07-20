009624eabfe9108d140bc0fa7d41da61606d2595
Matthew Menna <mattmenna@gmail.com>
2017-07-20 16:50:11 -0400

    Redoing Javascript from scratch screwed it up

--- lab19.js
+++ lab19.js
@@ -2,29 +2,36 @@ var cart = new Array();
 var itemName = new Array();
 var itemPrice = new Array();
 var cartlist;
-var grandTotal = 0;
+var grandTotal;
 var i;
 cart[0] = itemName;
 cart[1] = itemPrice;
 
 
 function addtocart(name, price) {
+    var cartString;
     cart[0].push(name);
     cart[1].push(price);
-    listCart();
-    totalCart();
+    document.getElementById("cart").innerHTML = cart[0] + " " + "<br>";
+
+    for(i=0; i<cart[1].length; i++) {
+        cartString += parseInt([1][i]);
+    }
+    document.getElementById("grandTotal").innerHTML = cartString;
+    //document.getElementById("cart").innerHTML = "<ul>"+cartlist + "</ul>";
+
+    //totalCart();
+
 }
 
 function listCart() {
-    cartlist += "<li>" + cart[0] + "<li>" + cart[1];
-    totalCart();
+
 }
 
-function totalCart(){
-    document.getElementById("cart").innerHTML = cartlist + "</ul";
-    grandTotal = 0 ;
-    for(i=0;i<cart[1].length;i++){
-       grandTotal += parseInt([1][i]);
+function totalCart() {
+    grandTotal = 0;
+    for (i = 0; i < cart[1].length; i++) {
+        grandTotal += cart[1][i];
     }
-    document.getElementById("grandTotal").innerHTML = grandTotal;
+    document.getElementById("grandTotal").innerHTML = cart[1][i];
 }
