28a8d8206e6e53fe293b020f19af0c6a2618f15a
Matthew Menna <mattmenna@gmail.com>
2017-07-20 12:42:13 -0400

    Initial commit

--- lab19.js
+++ lab19.js
@@ -1,12 +1,23 @@
 var cart = new Array();
+var itemName = new Array();
+var itemPrice = new Array();
+var cartlist;
+cart[0] = itemName;
+cart[1] = itemPrice;
 
 
-function addtocart(name, price){
-    cart.push(price);
-    updateCart();
+function addtocart(name, price) {
+    cart[0].push(name);
+    cart[1].push(price);
+    listCart();
+    totalCart();
 }
 
-function updateCart(){
-    document.getElementById("cart").innerHTML = cart.pop();
+function listCart() {
+    cartlist += "<ul> <li>" + cart[0].pop() + "<li>" + cart[1].pop();
+    totalCart();
 }
 
+function totalCart(){
+    document.getElementById("cart").innerHTML = cartlist;
+}
